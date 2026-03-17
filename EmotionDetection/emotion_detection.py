ba366d98a949afaae1d47f80782f3fa2d7ffe138
Ruslan Samchuk <ruslansamchuk@gmail.com>
2026-03-17 21:45:00 +0100

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,1 +1,29 @@
+import requests
+import json
 
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    myobj = {"raw_document": {"text": text_to_analyse}}
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(url, json=myobj, headers=header)
+
+    formatted_response = json.loads(response.text)
+
+    if "emotionPredictions" not in formatted_response:
+        return None
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    # Find dominant emotion automatically
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion
+    }
